c691aea01e4150cb3c38df9465305c5e35f728ea
luper <lachlann_whitehill_93@hotmail.com>
2018-09-10 10:39:33 +1000

    minor changes

--- js/quiz.js
+++ js/quiz.js
@@ -2,11 +2,16 @@
 var quizIdArray = [];
 var qTotal = 0;
 var qCorrect = 0;
-var qIncoorect = 0;
+var qIncorrect = 0;
+var currentQuizId = quizIdArray[0];
 
 $(function(){
     getQuizId();
 })
+window.addEventListener("load", function(e){
+	document.getElementById("quiz").addEventListener("submit", validateQuiz);
+});
+
 
 function getQuizId(){
     //make an ajax call with no params to get a list of quiz ID's
@@ -36,10 +41,6 @@ function getQuizConetent(quizId){
         dataType: "json",
         data: {q: quizId},
         success: function(data){
-            console.log(data.choices.A)
-            console.log(data.choices.B)
-            console.log(data.choices.C)
-            console.log(data.choices.D)
             displayQuiz(data.text, data.choices.A, data.choices.B, data.choices.C, data.choices.D);
         }
     })
@@ -49,23 +50,34 @@ function displayQuiz(qText, ansA, ansB, ansC, ansD){
     //Display the question text and answers to the html elements
     console.log(ansA)
     document.getElementById("quiz_question").textContent=qText;
-    document.getElementsByName("answer_a").innerHTML=ansA;
-    document.getElementsByName("answer_b").textContent=ansB;
-    document.getElementById("answer_c").textContent=ansC;
-    document.getElementById("answer_d").textContent=ansD;
+    $("label[for='answer_a']").text(ansA);
+    $("label[for='answer_b']").text(ansB);
+    $("label[for='answer_c']").text(ansC);
+    $("label[for='answer_d']").text(ansD);
     document.getElementById("quiz").classList.remove("hidden");
 }
 
-function verifyQuiz(qId, answer){
+function validateQuiz(e){
+    e.preventDefault();
+    var answer = $('input[name=answer]:checked').val();
+    console.log(answer)
+    var success = true;
     //make ajax request with q and a para to get a boolean value to verify the answer
     $.ajax({
         method: "POST",
         url: "http://turing.une.edu.au/~jbisho23/assignment2/quiz.php",
         dataType: "json",
-        data: {q: qId, a: answer},
+        data: {q: quizIdArray[0], a: answer},
         success: function(data){
-            console.log(data.q)
-            getQuizConetent(data.q);           
+            qTotal += 1;
+            if(data.correct){
+                qCorrect += 1;
+            } else {
+                qIncorrect += 1;
+            }
+            updateSideBar();
+            
+            return success;
         },
         error: function(data){
             alert(data.error);
@@ -75,4 +87,8 @@ function verifyQuiz(qId, answer){
 
 function updateSideBar(){
     //updates the side bar.  so we need a talley
+    document.getElementById("score").classList.remove("hidden");
+    document.getElementById("attempted").innerHTML="Attempted: " + qTotal;
+    document.getElementById("correct").innerHTML="Correct: " + qCorrect;
+    document.getElementById("incorrect").innerHTML="incorrect: " + qIncorrect;
 }
